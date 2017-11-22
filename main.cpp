447fe666171bcdec20e0326b0e031b4029340315
DHEERAJ12045 <33903023+DHEERAJ12045@users.noreply.github.com>
2017-11-22 21:18:37 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+    int sub;
+    cout << "enter the value of a";
+    cin >> a;
+    cout << "enter the vlaue of b";
+    cin >> b;
+    sum = a+b;
+    cout << "addition of numbers is " << sum << endl;
+    sub = a-b;
+    cout << "subtraction of numers is " << sub << endl;
+    return 0;
+
+}
